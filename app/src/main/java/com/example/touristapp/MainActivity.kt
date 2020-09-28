71c95cdc92cdce8ecd2bb11d261031ec54841c64
Kat28bot <katarzyna.oleksiewicz28@gmail.com>
2020-09-28 22:42:43 +0200

    app + try login +guide clown 2

--- app/src/main/java/com/example/touristapp/MainActivity.kt
+++ app/src/main/java/com/example/touristapp/MainActivity.kt
@@ -1,36 +1,71 @@
 package com.example.touristapp
 
+import android.R.layout
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.sip.SipSession
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.util.Log
+import android.view.*
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity()
 
 {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+   var blazenOn:Boolean = false
 
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val layoutInflater = baseContext
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView: View = layoutInflater.inflate(R.layout.bubble, null)
+        val popupWindow = PopupWindow(
+            popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+
+            if(blazenOn){
+                blazenOn=false
+               // fabula1.dismiss()
+                popupWindow.dismiss()
+            }else{
+                blazenOn=true
+                Log.i("blazen","blazen turnOn")
+
+                popupWindow.showAsDropDown(toolbar, 50, -500);
+
+            }
+
+
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -41,6 +76,15 @@ class MainActivity : AppCompatActivity()
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun showPopUp(){
+    val fabula1= Dialog(this)
+        fabula1.setCancelable(true)
+        fabula1.setCanceledOnTouchOutside(true)
+        fabula1.setContentView(R.layout.bubble)
+       // fabula1.window?.setBackgroundDrawable(ColorDrawable(T))
+        blazenOn=true
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
